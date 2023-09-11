36403e559201c04d9de7af5b95cc25c9133a0501
Fahad Abdul Qayyum <100997736+FahadAbdulQayyum@users.noreply.github.com>
2023-09-11 11:02:52 +0500

    Update index.js
    router explained in comment

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 // express imported
 const express = require('express')
+// Using this express.Router and saving it into a variable means making this directory router
 const router = express.Router()
 
 router.use('/auth', require('./users'))
