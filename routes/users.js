0502ccae30c27f199ca0b754be8fcdaed35863f1
Fahad Abdul Qayyum <100997736+FahadAbdulQayyum@users.noreply.github.com>
2023-09-12 10:51:27 +0500

    Update users.js
    function imported explained via comments

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 const express = require('express')
 // express.Router is stored in router variable
 const router = express.Router()
+// AuthLogin, Register and Product function called from users
 const { AuthLogin, Register, Product } = require('../controllers/users')
 
 router.post('/login', AuthLogin)
