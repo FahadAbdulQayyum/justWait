75ab21c1326053e0907b8961e2341a37f5ac7f1e
Fahad Abdul Qayyum <100997736+FahadAbdulQayyum@users.noreply.github.com>
2023-09-12 10:50:53 +0500

    Update users.js
    Router highlighted via comment

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-// express.js is exported
+// express.js is imported
 const express = require('express')
+// express.Router is stored in router variable
 const router = express.Router()
 const { AuthLogin, Register, Product } = require('../controllers/users')
 
