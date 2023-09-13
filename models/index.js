ee9cab11bb73c0750b4108c48c3d3a589b24d9a4
Fahad Abdul Qayyum <100997736+FahadAbdulQayyum@users.noreply.github.com>
2023-09-13 11:04:39 +0500

    Update index.js
    Index's file simple import and export explained via comment

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
+// imported Users here of this directory
 const Users = require('./Users')
 
+// That way simply importing and exporting helps us not need to write the whole users.js but simply call the directory name, the system automatically detect by checking the index file and act accordingly.
 module.exports = {
     Users
 }
